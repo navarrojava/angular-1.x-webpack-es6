62e99ab6b12e00362ebc3b0e3c17e126dc5c5a42
Navarro Ferreira <navarro@android-77a99sdsdsvds9-09.local>
2016-10-31 16:59:32 -0200

    adjust on structure folders

--- app/entry.js
+++ app/entry.js
@@ -3,6 +3,6 @@ import * as styles from './style.css';
 import './index.html';
 
 import angular  from 'angular';
-import example from './ExampleModule';
+import example from './example_module/ExampleModule';
 
 const app =  angular.module('app',['example']);
